165ea656745b4b26939a0f8fba5e49a58e9a3bc5
Joseph J Easson <josephj.easson@gmail.com>
2015-11-17 10:32:39 +0000

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('private route hit!');
+		next();
+	}, 
+	logger: function(req, res, next){
+		//new Date().toString()
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger); // this is at the application level
+
+app.get('/about',middleware.requireAuthentication, function(req, res){ //middleware at page level
+	res.send('About Us');	
+});
+
+app.use(express.static(__dirname + '/' +'/public')); //expose entire folder to the server
+
+app.listen(PORT, function(){
+	console.log('Express server started on port ' + PORT + ' !');
+});
