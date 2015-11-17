d114121f917149544962f27f04438a58a95a73b4
Joseph J Easson <josephj.easson@gmail.com>
2015-11-17 10:34:04 +0000

    Update language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger); // this is at the application level
 
 app.get('/about',middleware.requireAuthentication, function(req, res){ //middleware at page level
-	res.send('About Us');	
+	res.send('About Us!');	
 });
 
 app.use(express.static(__dirname + '/' +'/public')); //expose entire folder to the server
